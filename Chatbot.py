74eed79307fa5ede53cd34901f620372ecf25e80
Armin R. <66879529+datasmith-neef@users.noreply.github.com>
2023-09-22 13:35:04 +0000

    tutorialfunktion

--- Chatbot.py
+++ Chatbot.py
@@ -9,6 +9,27 @@ with st.sidebar:
 
 st.title("💬 Datasmith-GPT für Profis")
 st.caption("🚀 A streamlit chatbot powered by OpenAI LLM and Datasmith Office")
+
+# Dropdown-Menü hinzufügen
+role_options = ["Standard", "Tutorial-Writer"]
+selected_role = st.selectbox("Rolle auswählen:", role_options)
+
+# Den Prompt für die Rolle "Tutorial-Writer" festlegen
+if selected_role == "Tutorial-Writer":
+    tutorial_prompt = """
+    Du bist ein spezialisiertes Tutorial-Modul namens TutorialGPT. Deine Expertise besteht darin, detaillierte und leicht verständliche Tutorials zu generieren. Sobald ich dir meine Anforderungen schildere, schlüpfst du in die entsprechende Expertenrolle und erstellst ein vollständiges Tutorial.
+    
+    1. Frage mich nach dem gewünschten Thema des Tutorials. Warte auf meine Antwort!
+    2. Stelle mir basierend auf dem Thema aus Schritt 1 fünf Fragen, deren Antworten für dich ein perfektes Briefing zum Thema und Ziel des Tutorials darstellen.
+    3. Schlüpfe, basierend auf den Antworten aus Schritt 2, in die ideale Expertenrolle und bitte mich um Bestätigung. Warte auf meine Antwort!
+    4. Entwickle ein detailliertes Tutorial mit Titel, Kurzbeschreibung und strukturierter Gliederung. Bitte mich um Feedback und gehe erst nach meiner endgültigen Bestätigung zu Schritt 5.
+    5. Sobald ich mit dem Lehrplan zufrieden bin, beginne mit der kapitelweisen Erstellung des Tutorials. Frage mich nach jedem Kapitel um Feedback und fahre erst fort, wenn ich zufrieden bin.
+    
+    Beachte: Gehe immer davon aus, dass der Benutzer keine Vorkenntnisse zum Thema hat. Gestalte die Tutorials immer sehr detailliert und leicht nachvollziehbar.
+    """
+else:
+    tutorial_prompt = ""
+
 if "messages" not in st.session_state:
     st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
 
@@ -23,7 +44,8 @@ if prompt := st.chat_input():
     openai.api_key = openai_api_key
     st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
-    response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
+    # Den ausgewählten Prompt basierend auf der ausgewählten Rolle verwenden
+    response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=st.session_state.messages, prompt=tutorial_prompt)
     msg = response.choices[0].message
     st.session_state.messages.append(msg)
     st.chat_message("assistant").write(msg.content)
